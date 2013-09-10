e125c23536575c2dde521797f378f260e2b005b4
Sayyid Sofwan <sysofwan@umich.edu>
2013-09-10 16:52:25 -0400

    added scheduler and removal of get params

--- app/util/get_data.py
+++ app/util/get_data.py
@@ -6,47 +6,49 @@ from string import join
 import json
 from app import db
 from app.models.Content import Content
+from apscheduler.scheduler import Scheduler
 
+sched = Scheduler()
 
 def requestRssData(url):
 	content = feedparser.parse(url)
 	data = []
 	for i in content.entries:
 		dictData = {}
-		link = i.id
-		dictData['url'] = i.id
-		dictData['title'] = i.title
 		dictData['description'] = cleanSoupHtml(i.description).get_text()
+		dictData['title'] = i.title
 		dictData['timestamp'] = i.published_parsed
 
-		#request whole page
-		dictData['raw_html'] = requests.get(link).text
-		
-		#request facebook shares
-		dictData['facebook_shares'] = 0
-		fb_temp = requests.get('https://graph.facebook.com/'+link).json()
-		if 'shares' in fb_temp.keys():
-			dictData['facebook_shares'] = fb_temp['shares']
-		
-		#request twitter retweets
-		dictData['retweets'] = 0
-		twit_temp = requests.get('http://urls.api.twitter.com/1/urls/count.json?url='+link).json()
-		if 'count' in twit_temp.keys():
-			dictData['retweets'] = twit_temp['count']
+		link = i.id
+		pageRequst = requests.get(link)
 
-		# request reddit upvotes
-		dictData['upvotes'] = 0
-		try:
-			redditRedirectReq = requests.get('http://www.reddit.com/'+link)
-			reddit_url = redditRedirectReq.url
-			if reddit_url[-1] == '/':
-				reddit_url = reddit_url + '.json'
-			else:
-				reddit_url = reddit_url + '/.json'
-			reddit_temp = requests.get(reddit_url).json()
-			dictData['upvotes'] = reddit_temp[0]['data']['children'][0]['data']['ups']
-		except:
-			pass
+		# checks if page is available
+		if pageRequst.status_code == 404:
+			# if not, use rss link attr
+			link = i.link
+			pageRequst = requests.get(link)
+			# Goto next loop if page is still not found
+			if pageRequst.status_code == 404:
+				continue
+
+		if pageRequst.url != link:
+			tempLink = link
+			#	strips query parameters from url
+			link = (pageRequst.url).split('?')[0]
+
+		dictData['url'] = link
+		# request whole page
+		dictData['raw_html'] = pageRequst.text
+		
+		dictData['facebook_shares'] = getFacebookShares(link)
+		dictData['retweets'] = getRetweets(link)
+		dictData['upvotes'] = getUpvotes(link)
+
+		#	if tempLink exists (we got redirected), check the link we came from
+		if 'tempLink' in locals():
+			dictData['facebook_shares'] += getFacebookShares(tempLink)
+			dictData['retweets'] += getRetweets(tempLink)
+			dictData['upvotes'] += getUpvotes(tempLink)
 
 		#use beautiful soup to parse html
 		soup = cleanSoupHtml(dictData['raw_html'])
@@ -73,16 +75,45 @@ def requestRssData(url):
 		time.sleep(0.5)
 	return data
 
-def loadDatabse():
+def getFacebookShares(url):
+	fb_temp = requests.get('https://graph.facebook.com/'+url).json()
+	if 'shares' in fb_temp:
+		return fb_temp['shares']
+	return 0
+
+def getRetweets(url):
+	twit_temp = requests.get('http://urls.api.twitter.com/1/urls/count.json?url='+url).json()
+	if 'count' in twit_temp:
+		return twit_temp['count']
+	return 0
+
+def getUpvotes(url):
+	try:
+		redditRedirectReq = requests.get('http://www.reddit.com/'+url)
+		reddit_url = redditRedirectReq.url
+		if reddit_url[-1] == '/':
+			reddit_url = reddit_url + '.json'
+		else:
+			reddit_url = reddit_url + '/.json'
+		reddit_temp = requests.get(reddit_url).json()
+		return reddit_temp[0]['data']['children'][0]['data']['ups']
+	except:
+		return 0
+
+@sched.interval_schedule(hours=1)
+def loadDatabase():
 	rssFeeds = ['http://rss.cnn.com/rss/cnn_topstories.rss',
 				'http://feeds.gawker.com/lifehacker/full',
-				'http://feeds.wired.com/wired/index']
+				'http://feeds.wired.com/wired/index',
+				'http://feeds2.feedburner.com/time/topstories',
+				'http://feeds.gawker.com/gizmodo/full',
+				'http://www.theverge.com/rss/frontpage',
+				'http://feeds.feedburner.com/TechCrunch/']
 	for url in rssFeeds:
 		data = requestRssData(url)
 		for content in data:
 			Content.getOrCreateContent(db.session,**content)
 		db.session.commit()
-		
 
 # accepts a html string and returns a soup object with only contents
 def cleanSoupHtml(html):
--- run.py
+++ run.py
@@ -1,3 +1,6 @@
 #!flask/bin/python
 from app import app
+from app.util.get_data import sched
+
+sched.start()
 app.run(debug = True)
