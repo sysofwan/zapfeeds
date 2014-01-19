7718e619f503b2d2207af8865946b15fbea698e1
Sayyid Sofwan <sysofwan@umich.edu>
2014-01-19 09:13:24 -0800

    hello

--- config.py
+++ config.py
@@ -1,10 +1,31 @@
-import os
-basedir = os.path.abspath(os.path.dirname(__file__))
+var connect = require('connect'),
+    sharejs = require('share').server
+    connectRoute = require('connect-route')
+    render = require('connect-render');
 
-if os.environ.get('DATABASE_URL') is None:
-    SQLALCHEMY_DATABASE_URI = 'postgresql://sysofwan@localhost/aggregator_daddy'
-else:
-    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
-SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
-CSRF_ENABLED = True
-SECRET_KEY = 'it-is-a-secret'
+var server = connect(
+	  render({root: __dirname + '/public', layout: 'index.html', cache: wd false}),
+      connect.logger(),
+      connect.static(__dirname + '/public')
+    );
+
+server.use(connectRoute(function(router) {
+	router.get('/code/:name', function (req, res, next) {
+    	res.render('index.html');
+    });
+	router.get('/', function (req, res, next) {
+        res.end('index');
+        console.log('hi!!');
+    });
+}));
+var options = {db: {type: 'redis'}}; // See docs for options. {type: 'redis'} to enable persistance.
+
+// Attach the sharejs REST and Socket.io interfaces to the server
+sharejs.attach(server, options);
+
+server.listen(8000, function(){
+    console.log('Server running at http://127.0.0.1:8000/');
+});
+/// Hi!!! asdasd qwqdwdqwdewd
+
+// edit over hereuifhuibgfibxfjbgfxiubdfipiiwedwedwedwedwed
