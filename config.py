39b360c1b3f618bc06632ea75b1238ddfde60dbc
Sayyid Sofwan <sysofwan@umich.edu>
2014-01-25 02:07:32 -0500

    changed default postgres db

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 if os.environ.get('DATABASE_URL') is None:
-    SQLALCHEMY_DATABASE_URI = 'postgresql://sysofwan@localhost/aggregator_daddy'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://apps@localhost/aggregator_daddy'
 else:
     SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
