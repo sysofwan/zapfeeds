56475d57480f567cb4c2755f902a5320b3707fee
Lidsky <hmr1808@gmail.com>
2014-02-19 21:04:40 -0500

    Temporarily fixed ranking by adding majority of weight to initial shares over predicted shares

--- app/background_services/ranking/rank.py
+++ app/background_services/ranking/rank.py
@@ -16,10 +16,21 @@ def unweighted_score(social_score):
     order = log(social_score, 10)
     return round(order, 7)
 
-def rank_content(content):
+def rank_content(content, weight=0.1):
+    """
+    @todo: improve algorithm long term
+    """
     age = datetime.utcnow() - content.timestamp
-    total_score = content.predicted_shares
+    initial_social_shares = get_initial_social_shares(content)
+    total_score = initial_social_shares + (content.predicted_shares * weight)
     if age < timedelta(days=3):
         return score(total_score, content.timestamp)
     else:
         return unweighted_score(total_score)
+
+
+def get_initial_social_shares(content_obj):
+    facebook_likes = content_obj.social_shares[0].facebook_shares
+    retweets = content_obj.social_shares[0].retweets
+    upvotes = content_obj.social_shares[0].upvotes
+    return facebook_likes + retweets + upvotes
